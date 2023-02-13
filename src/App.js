3c5f235e1c9b7ccd02d03553582746fc82885e9b
MIH\pongsit <pongsit.po@outlook.com>
2023-02-13 16:40:57 +0700

    update v1.1

--- src/App.js
+++ src/App.js
@@ -13,16 +13,22 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  Stack,
+  TextField,
+  Button,
 } from '@mui/material';
 
-let HNTimer = [];
+let HNStatus = [];
+let filterHNStatus = [];
+let tableState = "showAll";
+let findHN = "";
 
 export default function App() {
 
   const [curDate, setCurDate] = useState('');
   const [curTime, setCurTime] = useState('');
 
-  const [HNStatus, setHNStatus] = useState([]);
+  const isSkip = (value) => value !== '';
 
   useEffect(() => {
 
@@ -31,19 +37,19 @@ export default function App() {
     }, 1000);
     const interval2 = setInterval(() => {
       refreshData();
-    }, 20000);
+    }, 5000);
     refreshData();
     return () => {
       clearInterval(interval);
       clearInterval(interval2)
     };
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function updateTime() {
     const date = new Date();
-    setCurDate(date.toLocaleDateString('th-TH', {
+    setCurDate(date.toLocaleDateString('en-gb', {
       year: 'numeric',
       month: 'long',
       day: 'numeric',
@@ -56,7 +62,7 @@ export default function App() {
     fetch(`http://${process.env.REACT_APP_host}:${process.env.REACT_APP_QDSPort}/api/qds/gethnstatus`)
       .then((response) => response.json())
       .then((data) => {
-        addHNTimer(data);
+        addHNStatus(data);
       })
       .catch((error) => {
         if (error.name === "AbortError") {
@@ -68,30 +74,57 @@ export default function App() {
       });
   }
 
-  async function addHNTimer(newData) {
+  async function addHNStatus(newData) {
 
-    let tempHNTimer = [];
+    let tempHNStatus = [];
     const date = new Date();
 
-    if (HNTimer.length !== 0) {
+    if (HNStatus.length !== 0) {
       for (let i = 0; i < newData.length; i += 1) {
-        for (let l = 0; l < HNTimer.length; l += 1) {
-          if (newData[i].VNSEQ === HNTimer[l].VNSEQ) {
-            tempHNTimer.push({
-              VNSEQ: newData[i].VNSEQ,
-              STATUS: newData[i].STATUS,
-              TIME: await HNTimer[l].STATUS === newData[i].STATUS ? HNTimer[l].TIME : date.toLocaleString(),
-              WAIT_TIME: await HNTimer[l].STATUS === newData[i].STATUS ? "" + new Date(date.getTime() - new Date(HNTimer[l].TIME).getTime() - 24100000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false, }) : "00:00",
-            });
+        for (let l = 0; l < HNStatus.length; l += 1) {
+          if (newData[i].VNSEQ === HNStatus[l].VNSEQ) {
+            if (HNStatus[l].FIN_FLAG !== 4) {
+              if (newData[i].STATUS === "จ่ายยา" || newData[i].STATUS === "ชำระเงิน(คนไข้ไม่มียา)") {
+                console.log("finishing detect!! counter = " + HNStatus[l].FIN_FLAG + 1);
+                tempHNStatus.push({
+                  HN: newData[i].HN,
+                  DIVISION: newData[i].DIVISION,
+                  REG_TIME: newData[i].TIME,
+                  VNSEQ: newData[i].VNSEQ,
+                  STATUS: newData[i].STATUS,
+                  TIME: await HNStatus[l].STATUS === newData[i].STATUS ? HNStatus[l].TIME : date.toLocaleString(),
+                  WAIT_TIME: await HNStatus[l].STATUS === newData[i].STATUS ? new Date(date.getTime() - new Date(HNStatus[l].TIME).getTime() - 25200000).toLocaleTimeString('th-TH', { minute: '2-digit', hour12: false, }) + (parseInt(new Date(date.getTime() - new Date(HNStatus[l].TIME).getTime() - 25200000).toLocaleTimeString('th-TH', { hour: '2-digit', hour12: false, })) * 60) : 0,
+                  FIN_FLAG: HNStatus[l].FIN_FLAG + 1,
+                });
+              }
+              else {
+                tempHNStatus.push({
+                  HN: newData[i].HN,
+                  DIVISION: newData[i].DIVISION,
+                  REG_TIME: newData[i].TIME,
+                  VNSEQ: newData[i].VNSEQ,
+                  STATUS: newData[i].STATUS,
+                  TIME: await HNStatus[l].STATUS === newData[i].STATUS ? HNStatus[l].TIME : date.toString(),
+                  WAIT_TIME: await HNStatus[l].STATUS === newData[i].STATUS ? parseInt(new Date(date.getTime() - new Date(HNStatus[l].TIME).getTime() - 25200000).toLocaleTimeString('th-TH', { minute: '2-digit', hour12: false, })) + (parseInt(new Date(date.getTime() - new Date(HNStatus[l].TIME).getTime() - 25200000).toLocaleTimeString('th-TH', { hour: '2-digit', hour12: false, })) * 60) : 0,
+                  FIN_FLAG: 0,
+                });
+              }
+            }
             break;
           }
-          else if (l === HNTimer.length - 1) {
-            tempHNTimer.push({
-              VNSEQ: newData[i].VNSEQ,
-              STATUS: newData[i].STATUS,
-              TIME: date.toLocaleString(),
-              WAIT_TIME: "00:00",
-            });
+          else if (l === HNStatus.length - 1) {
+            if (newData[i].STATUS !== "จ่ายยา" && newData[i].STATUS !== "ชำระเงิน(คนไข้ไม่มียา)") {
+              tempHNStatus.push({
+                HN: newData[i].HN,
+                DIVISION: newData[i].DIVISION,
+                REG_TIME: newData[i].TIME,
+                VNSEQ: newData[i].VNSEQ,
+                STATUS: newData[i].STATUS,
+                TIME: date.toString(),
+                WAIT_TIME: 0,
+                FIN_FLAG: 0,
+              });
+            }
           }
 
         }
@@ -99,17 +132,46 @@ export default function App() {
     }
     else {
       for (let i = 0; i < newData.length; i += 1) {
-        tempHNTimer.push({
-          VNSEQ: newData[i].VNSEQ,
-          STATUS: newData[i].STATUS,
-          TIME: date.toLocaleString(),
-          WAIT_TIME: "00:00",
-        });
+        if (newData[i].STATUS !== "จ่ายยา" && newData[i].STATUS !== "ชำระเงิน(คนไข้ไม่มียา)") {
+          tempHNStatus.push({
+            HN: newData[i].HN,
+            DIVISION: newData[i].DIVISION,
+            REG_TIME: newData[i].TIME,
+            VNSEQ: newData[i].VNSEQ,
+            STATUS: newData[i].STATUS,
+            TIME: date.toString(),
+            WAIT_TIME: 0,
+            FIN_FLAG: 0,
+          });
+        }
       }
     }
-    HNTimer = tempHNTimer;
-    await setHNStatus(newData);
 
+    HNStatus = tempHNStatus;
+    await setFilterHNStatus();
+
+  }
+
+  async function setFilterHNStatus(){
+    if(tableState === "showAll"){
+      filterHNStatus = HNStatus;
+    }
+    else if(tableState === "findHN"){
+      filterHNStatus = HNStatus.filter(dt => (dt.HN).includes(findHN));
+    }
+  }
+
+  const handleFindHN = () => {
+    tableState = "findHN";
+    findHN = document.getElementById('HNCode').value;
+    setFilterHNStatus();
+  };
+
+  const handleShowAll = () => {
+    tableState = "showAll";
+    findHN = "";
+    document.getElementById("HNCode").value = '';
+    setFilterHNStatus();
   }
 
   return (
@@ -118,57 +180,68 @@ export default function App() {
       <Container maxWidth="lg">
 
         <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Typography variant="h4" gutterBottom>
-                Dashboard
-              </Typography>
+          <Grid container spacing={1}>
+            <Grid item xs={8}>
+              <img width="300" src={`${process.env.PUBLIC_URL}/img/logo.png`} alt="logo" />
+            </Grid>
+            <Grid item xs={4} align='right' sx={{ display: 'flex', alignItems: 'end', justifyContent: 'flex-end', }}>
+              <Stack>
+                <Typography variant="h4" gutterBottom>
+                  Patient Status
+                </Typography>
+                <Typography gutterBottom>
+                  {`${curDate} ${curTime}`}
+                </Typography>
+              </Stack>
             </Grid>
             <Grid item xs={8}>
+              <Stack direction="row" spacing={1}>
+                <TextField id="HNCode" name="HNCode" label="HN code" size='small' />
+                <Button variant="contained" sx={{ width: 100 }} onClick={handleFindHN} >ค้นหา</Button>
+                <Button variant="contained" sx={{ width: 120 }} onClick={handleShowAll} >แสดงทั้งหมด</Button>
+              </Stack>
+            </Grid>
+            <Grid item xs={4}>
 
             </Grid>
-            <Grid item xs={4} align='right'>
-              {`วันที่ ${curDate} ${curTime} น.`}
-            </Grid>
-            <Grid item xs={12}>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <Grid item xs={12} >
+              <TableContainer component={Paper} style={{ overflowX: "initial" }}>
+                <Table stickyHeader sx={{ minWidth: 650 }} aria-label="sticky table" >
                   <TableHead>
                     <TableRow>
-                      <TableCell>HN</TableCell>
-                      <TableCell>หน่วยงาน</TableCell>
-                      <TableCell>ลงทะเบียน</TableCell>
-                      <TableCell>ซักประวัติ</TableCell>
-                      <TableCell>พบแพทย์</TableCell>
-                      <TableCell>รอผลตรวจ</TableCell>
-                      <TableCell>ชำระเงิน</TableCell>
-                      <TableCell>รับยา</TableCell>
-                      <TableCell>กลับบ้าน</TableCell>
-                      <TableCell>เวลาคอย</TableCell>
+                      <TableCell align='center' ><img width="76" src={`${process.env.PUBLIC_URL}/img/table_hn.jpg`} alt="HN" /></TableCell>
+                      <TableCell align='center' ><img width="76" src={`${process.env.PUBLIC_URL}/img/table_division.jpg`} alt="หน่วยงาน" /></TableCell>
+                      <TableCell align='center' ><img width="76" src={`${process.env.PUBLIC_URL}/img/table_register.jpg`} alt="ลงทะเบียน" /></TableCell>
+                      <TableCell align='center' ><img width="76" src={`${process.env.PUBLIC_URL}/img/table_historytake.jpg`} alt="ซักประวัติ" /></TableCell>
+                      <TableCell align='center' ><img width="76" src={`${process.env.PUBLIC_URL}/img/table_seedoctor.jpg`} alt="พบแพทย์" /></TableCell>
+                      <TableCell align='center' ><img width="76" src={`${process.env.PUBLIC_URL}/img/table_waitresult.jpg`} alt="รอผลตรวจ" /></TableCell>
+                      <TableCell align='center' ><img width="76" src={`${process.env.PUBLIC_URL}/img/table_makepay.jpg`} alt="ชำระเงิน" /></TableCell>
+                      <TableCell align='center' ><img width="76" src={`${process.env.PUBLIC_URL}/img/table_getmed.jpg`} alt="รับยา" /></TableCell>
+                      <TableCell align='center' ><img width="76" src={`${process.env.PUBLIC_URL}/img/table_finished.jpg`} alt="กลับบ้าน" /></TableCell>
+                      <TableCell><img width="76" src={`${process.env.PUBLIC_URL}/img/table_waittime.jpg`} alt="เวลาคอย" /></TableCell>
                     </TableRow>
                   </TableHead>
-                  <TableBody>
-                    {HNStatus.map((row) => (
+                  <TableBody sx={{ '& tr:nth-of-type(odd)': { backgroundColor: 'grey.300', }, }}>
+                    {filterHNStatus.map((row) => (
                       <TableRow
                         key={row.VNSEQ}
                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                       >
                         <TableCell>{row.HN}</TableCell>
-                        <TableCell>{row.DIVISION}</TableCell>
-                        <TableCell>{row.TIME}</TableCell>
-                        <TableCell align='center' >{row.STATUS === "ลงทะเบียน" ? "X" : ""}</TableCell>
-                        <TableCell align='center' >{row.STATUS === "รอพบแพทย์" ? "X" : ""}</TableCell>
-                        <TableCell align='center' >{row.STATUS === "รอผล" ? "X" : ""}</TableCell>
-                        <TableCell align='center' >{row.STATUS === "จบการรักษา" ? "X" : ""}</TableCell>
-                        <TableCell align='center' >{row.STATUS === "ชำระเงิน" ? "X" : ""}</TableCell>
-                        <TableCell align='center' >{row.STATUS === "จ่ายยา" || row.STATUS === "ชำระเงิน(คนไข้ไม่มียา)" ? "X" : ""}</TableCell>
-                        <TableCell>{HNTimer?.find(o => o.VNSEQ === row.VNSEQ)?.WAIT_TIME} นาที</TableCell>
+                        <TableCell align='center' >{row.DIVISION}</TableCell>
+                        <TableCell align='center' >{row.REG_TIME}</TableCell>
+                        <TableCell align='center' >{row.STATUS === "ลงทะเบียน" ? <img width="26" src={`${process.env.PUBLIC_URL}/img/Green_dot.svg`} alt="กลับบ้าน" /> : ""}</TableCell>
+                        <TableCell align='center' >{row.STATUS === "รอพบแพทย์" ? <img width="26" src={`${process.env.PUBLIC_URL}/img/Green_dot.svg`} alt="กลับบ้าน" /> : ""}</TableCell>
+                        <TableCell align='center' >{row.STATUS === "รอผล" ? <img width="26" src={`${process.env.PUBLIC_URL}/img/Green_dot.svg`} alt="กลับบ้าน" /> : ""}</TableCell>
+                        <TableCell align='center' >{row.STATUS === "จบการรักษา" ? <img width="26" src={`${process.env.PUBLIC_URL}/img/Green_dot.svg`} alt="กลับบ้าน" /> : ""}</TableCell>
+                        <TableCell align='center' >{row.STATUS === "ชำระเงิน" ? <img width="26" src={`${process.env.PUBLIC_URL}/img/Green_dot.svg`} alt="กลับบ้าน" /> : ""}</TableCell>
+                        <TableCell align='center' >{row.STATUS === "จ่ายยา" || row.STATUS === "ชำระเงิน(คนไข้ไม่มียา)" ? <img width="26" src={`${process.env.PUBLIC_URL}/img/Green_dot.svg`} alt="กลับบ้าน" /> : ""}</TableCell>
+                        <TableCell>{row.WAIT_TIME} min{row.WAIT_TIME > 1 ? "s" : ""}</TableCell>
                       </TableRow>
                     ))}
                   </TableBody>
                 </Table>
               </TableContainer>
-
             </Grid>
           </Grid>
         </Box>
